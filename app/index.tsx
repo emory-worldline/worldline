7d9e5f92fc3034ddda2fe94f788f80e75a53d8ac
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-09-30 20:00:52 -0400

    added component for previewing photo metadata (id, time, and location)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
 import { Text, View } from "react-native";
+import PhotoMetadata from "@/components/PhotoMetadata";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <PhotoMetadata />
   );
 }
