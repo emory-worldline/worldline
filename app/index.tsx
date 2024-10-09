5cb17ce463ff6147a2d07bca31211b882737cc0d
beckmiller <160279465+beckmiller@users.noreply.github.com>
2024-10-09 14:19:09 -0400

    temp save changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import PhotoMetadata from "@/components/PhotoMetadata";
+import MapComponent from "@/components/MapComponent"
 
-export default function Index() {
-  return <PhotoMetadata />;
-}
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MapComponent />
+      <PhotoMetadata />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1
+  },
+});
+
+export default App;
+
+// export default function Index() {
+//   return <PhotoMetadata />;
+// }
