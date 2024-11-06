e744592ab1ed38288b1c7ca65b678842275a6866
Rohan Sinha <rohansinha1000@gmail.com>
2024-11-06 00:13:39 -0500

    added link to tabs on setup page

--- app/setup.tsx
+++ app/setup.tsx
@@ -1,6 +1,32 @@
 import React from "react";
 import LibraryAnalyzer from "@/components/LibraryStats";
+import { Link } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Setup() {
-  return <LibraryAnalyzer />;
+  return (
+    <View>
+      <View style={styles.buttonContainer}>
+        <LibraryAnalyzer />
+      </View>
+      <Link href={"/(tabs)"} style={styles.link}>
+        Next Page
+      </Link>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  link: {
+    marginTop: 200,
+    marginLeft: 165,
+  },
+  buttonContainer: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    // borderWidth: 3,
+    width: 200,
+    marginLeft: 100,
+  },
+});
